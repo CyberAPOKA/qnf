d42faa7fe2b50ffea67b60f45044481991f60fd3
CyberAPOKA <oficialsteffens@hotmail.com>
2026-07-15 12:46:27 -0300

    fix: configure PWA metadata

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,23 +2,29 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 
-        <meta name="theme-color" content="#111111">
         <meta name="description" content="Organização das partidas de futsal do QNF">
+        <meta name="theme-color" content="#111111">
+        <meta name="application-name" content="QNF">
+
         <meta name="mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="default">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="apple-mobile-web-app-title" content="QNF">
 
         <title inertia>{{ config('app.name', 'QNF') }}</title>
 
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png">
-        <link rel="manifest" href="/build/manifest.webmanifest">
+        <link rel="manifest" href="{{ asset('build/manifest.webmanifest') }}">
+        <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
+        <link rel="icon" href="{{ asset('pwa-192x192.png') }}" type="image/png" sizes="192x192">
+        <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon-180x180.png') }}" sizes="180x180">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet">
+        <link
+            href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap"
+            rel="stylesheet"
+        >
 
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
