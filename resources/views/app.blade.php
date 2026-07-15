17f28be7892b33e508b30482c337db3db9a3134c
CyberAPOKA <oficialsteffens@hotmail.com>
2026-07-15 12:36:24 -0300

    feat: configure PWA metadata

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,17 +4,27 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', '2026') }}</title>
+        <meta name="theme-color" content="#111111">
+        <meta name="description" content="Organização das partidas de futsal do QNF">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="default">
+        <meta name="apple-mobile-web-app-title" content="QNF">
 
-        <!-- Fonts -->
+        <title inertia>{{ config('app.name', 'QNF') }}</title>
+
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png">
+        <link rel="manifest" href="/build/manifest.webmanifest">
+
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet">
 
-        <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
+
     <body class="font-sans antialiased">
         @inertia
     </body>
